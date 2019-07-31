6beef3b727f5da8510ec5eaca15c3728f792d6ad
Milan Burda <milan.burda@gmail.com>
2019-07-31 23:24:40 +0200

    refactor: convert ipc-renderer.ts to TypeScript

--- lib/renderer/api/ipc-renderer.ts
+++ lib/renderer/api/ipc-renderer.ts
@@ -1,10 +1,8 @@
-'use strict'
-
 const { ipc } = process.electronBinding('ipc')
 const v8Util = process.electronBinding('v8_util')
 
 // Created by init.js.
-const ipcRenderer = v8Util.getHiddenValue(global, 'ipc')
+export const ipcRenderer = v8Util.getHiddenValue<Electron.IpcRenderer>(global, 'ipc')
 const internal = false
 
 ipcRenderer.send = function (channel, ...args) {
@@ -23,15 +21,9 @@ ipcRenderer.sendTo = function (webContentsId, channel, ...args) {
   return ipc.sendTo(internal, false, webContentsId, channel, args)
 }
 
-ipcRenderer.sendToAll = function (webContentsId, channel, ...args) {
-  return ipc.sendTo(internal, true, webContentsId, channel, args)
-}
-
 ipcRenderer.invoke = function (channel, ...args) {
-  return ipc.invoke(channel, args).then(({ error, result }) => {
+  return ipc.invoke(internal, channel, args).then(({ error, result }) => {
     if (error) { throw new Error(`Error invoking remote method '${channel}': ${error}`) }
     return result
   })
 }
-
-module.exports = ipcRenderer
